b2e2645c765e8bd91a531a44d7855da3f55e396f
messat <148489126+messat@users.noreply.github.com>
2025-01-23 15:35:47 +0000

    This commit connects API Route from back-end server to get all events. Utilised Axios library to fetch data from localhost server.

--- src/components/AllEvents.jsx
+++ src/components/AllEvents.jsx
@@ -4,14 +4,32 @@ import ButtonGroup from '@mui/joy/ButtonGroup';
 import Paper from '@mui/material/Paper';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Groups2Icon from '@mui/icons-material/Groups2';
+import { useEffect } from 'react';
+import { getAllEvents } from '../API server/api';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AllEvents (){
+  const [allEvents, setAllEvents] = useState([]) 
+
+  useEffect(() => {
+      (async () => {
+        try {
+          const eventsArr = await getAllEvents()
+          console.log(eventsArr)
+          setAllEvents(eventsArr)
+          return eventsArr
+        } catch (err) {
+          console.error(err, "From all events")
+        }
+      })()
+  }, [])
     
     return <Box>
         
         <Grid2 container spacing={2} sx={{marginX: {xs: "2.5em", }, marginTop: "2.7em", marginBottom: "2.5em"}}>
-                {eventsArr.map((event) => (
-            <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3}} key={event.id}>
+                {allEvents.map((event) => (
+            <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3}} key={event._id}>
                     <Paper elevation={6} spacing={5} item="true" sx={{maxHeight: "700px", minHeight: "435px", mb: 2}}>
 
                         <img src={event.event_img_url} alt='Event Image' className='eventImage'/>
@@ -22,7 +40,7 @@ export default function AllEvents (){
                         <Typography variant='h6' sx={{mx: 2, display: "inline-block"}}>{event.price ? "£" + event.price: "FREE" }</Typography>
 
                         <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
-                        <Typography sx={{display: "inline-block", mt: -0.7, mr: 2, typography: { xs: 'h6', sm: 'body1', md: 'button'}}} variant='h6'>{event.event_last + " hr"}</Typography>
+                        <Typography sx={{display: "inline-block", mt: -0.7, mr: 2, typography: { xs: 'h6', sm: 'body1', md: 'button'}}} variant='h6'>{event.duration + " hr"}</Typography>
                         <AccessTimeIcon sx={{mr: 2, display: "inline-block", mt: -0.2}}/>
                         </div>
 
@@ -38,7 +56,7 @@ export default function AllEvents (){
                         variant="solid"
                         sx={{ '--ButtonGroup-radius': '40px', mx: 2, mt: -5.2}}
                         >
-                        <Button>View event</Button>
+                        <Button component={Link} to={`/event/${event._id}`}>View event</Button>
                         </ButtonGroup>
                     </Paper>
             </Grid2>
